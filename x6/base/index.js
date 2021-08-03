320d5ed46400ae36072dd839ded99e29f3d7fb45
zhengxixuan <930042468@qq.com>
2021-08-03 13:13:23 +0800

    change

--- x6/base/index.js
+++ x6/base/index.js
@@ -22,27 +22,102 @@ const data = {
 			}
 		},
 		{
-			shape: 'ellipse',
+			shape: 'rect',
 			id: 'node2', // String，节点的唯一标识
-			x: 40, // Number，必选，节点位置的 x 值
+			x: 200, // Number，必选，节点位置的 x 值
 			y: 100, // Number，必选，节点位置的 y 值
-			width: 80, // Number，可选，节点大小的 width 值
-			height: 40, // Number，可选，节点大小的 height 值
-			label: 'world' // String，节点标签
+			width: 200, // Number，可选，节点大小的 width 值
+			height: 100, // Number，可选，节点大小的 height 值
+			label: 'world', // String，节点标签
+			ports: {
+				groups: {
+					group1: {
+						markup: {
+							tagName: 'circle',
+							selector: 'circle',
+							attrs: {
+								r: 10,
+								fill: '#fff',
+								stroke: '#000'
+							}
+						},
+						attrs: {
+							circle: {
+								r: 6
+							}
+						},
+						zIndex: 1,
+						position: {
+							name: 'right',
+							args: {}
+						},
+						label: {
+							markup: {
+								tagName: 'text',
+								selector: 'text',
+								attrs: {
+									fill: '#000'
+								}
+							}
+						}
+					}
+				},
+				items: [
+					{
+						id: 'port1',
+						group: 'group1',
+						attrs: {
+							text: {
+								text: 'port1'
+							}
+						}
+					},
+					{
+						id: 'port2',
+						group: 'group1'
+					},
+					{
+						id: 'port3',
+						attrs: {
+							circle: {
+								r: 5
+							}
+						}
+					}
+				]
+			}
 		}
 	],
 	edges: [
 		{
-			source: 'node1', // String，必须，起始节点 id
-			target: 'node2', // String，必须，目标节点 id
-			vertices: [
-				{ x: 100, y: 200 },
-				{ x: 300, y: 120 }
-			],
-			router: {
-				name: 'orth',
-				args: {}
+			source: 'node1',
+			target: {
+				cell: 'node2', // 哪个节点
+				port: 'port1' // 哪个链接桩
 			},
+
+			labels: [
+				{
+					attrs: { label: { text: '连接', fill: '#61C0BF' } }
+				}
+			],
+			attrs: {
+				label: {
+					text: '连接',
+					fill: '#61C0BF'
+				},
+				line: {
+					stroke: '#61C0BF'
+				}
+			}
+		},
+		{
+			source: 'node1',
+			target: {
+				cell: 'node2', // 哪个节点
+				port: 'port2' // 哪个链接桩
+			},
+
 			labels: [
 				{
 					attrs: { label: { text: '连接', fill: '#61C0BF' } }
@@ -73,7 +148,7 @@ const graph = new X6.Graph({
 		color: '#BBDED6' // 设置画布背景颜色
 	},
 	grid: {
-		size: 60, // 网格大小 10px
+		size: 1, // 网格大小 10px
 		visible: true // 渲染网格背景
 	}
 })
