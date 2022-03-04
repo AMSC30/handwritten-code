68010bac7f122325607cd2c3e390597c45fceb48
AMSC30 <930042468@qq.com>
2022-03-04 22:30:01 +0800

    手写vuex

---
+++ vuex-v2实现/vuex/module/module-collection.js
@@ -0,0 +1,34 @@
+import { Module } from './module.js'
+
+export default class ModuleCollection {
+    constructor(rawModule) {
+        this.registerModule([], rawModule)
+    }
+    get(path) {
+        return path.reduce((module, path) => {
+            return module[path]
+        }, this.root)
+    }
+    getNameSpace(path) {
+        let module = this.root
+        return path.reduce((path, key) => {
+            module = module.getChild(path)
+            return path + (module && module.rawModule.nameSpaced ? key + '/' : '')
+        }, '')
+    }
+    registerModule(path, rawModule) {
+        const module = new Module(rawModule)
+
+        if (!path.length) {
+            this.root = module
+        } else {
+            const parentModule = this.get(path.slice(0, path.length - 1))
+            parentModule.addChild(path[path.length - 1], module)
+        }
+        if (rawModule.modules) {
+            Object.keys(rawModule.modules).forEach(key => {
+                this.registerModule(path.concat(key), rawModule.modules[key])
+            })
+        }
+    }
+}
---
+++ vuex-v2实现/vuex/store.js
@@ -0,0 +1,71 @@
+import ModuleCollection from './module/module-collection.js'
+export class Store {
+    constructor(options) {
+        this.actions = Object.create(null)
+        this.mutations = Object.create(null)
+        this.getters = Object.create(null)
+
+        this.wrappedGetters = Object.create(null)
+
+        this.subscribes = Object.create(null)
+        this.subscribeActions = Object.create(null)
+
+        this.modules = new ModuleCollection(options)
+
+        const rootState = this.modules.root
+
+        installModule(this, rootState, [], this.modules.root)
+
+        resetStoreVm(this)
+    }
+    commit() {}
+    dispatch() {}
+    subscribe() {}
+    subscribeAction() {}
+}
+function installModule(store, rootState, path, module) {
+    const isRoot = !path.length
+    const nameSpace = store.modules.getNameSpace(path)
+
+    // 处理state
+    if (!isRoot) {
+        const parentState = getNestedState(rootState, path.slice(0, -1))
+        const moduleName = path[path.length - 1]
+        Vue.set(parentState, moduleName, module.state)
+    }
+
+    // 生成local
+    const local = (module.context = makeLocal(store, nameSpace, path))
+
+    // 处理mutation
+    module.mutationForeach((mutation, key) => {
+        const newSpace = nameSpace + key
+        registerMutation(store, newSpace, mutation, local)
+    })
+    // 处理action
+    module.actionForeach((action, key) => {
+        const newSpace = nameSpace + key
+        registerAction(store, newSpace, action, local)
+    })
+    // 处理getter
+    module.actionGetters((getter, key) => {
+        const newSpace = nameSpace + key
+        registerGetter(store, newSpace, getter, local)
+    })
+    // 递归安装
+    module.childForeach((module, key) => {
+        installModule(store, rootState, path.concat(key), module)
+    })
+}
+
+function getNestedState(state, path) {
+    return path.reduce((state, key) => {
+        return state[key]
+    }, state)
+}
+function registerMutation() {}
+function registerAction() {}
+function registerGetter() {}
+function resetStoreVm() {}
+
+function makeLocal() {}
