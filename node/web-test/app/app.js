f4138ce939ab3db60a4e73abf2ff64fd5953092b
zhengxixuan <930042468@qq.com>
2022-06-28 18:01:56 +0800

    koa源码梳理

--- node/web-test/app/app.js
+++ node/web-test/app/app.js
@@ -1,4 +1,5 @@
-const Koa = require('koa')
+// const Koa = require('koa')
+const Koa = require('../../koa相关库源码/koa/lib/application.js')
 const routerRegister = require('../router/router')
 const app = new Koa()
 
